1682033284c2f3b7927185bc7cb5afef2214f4b2
Dasiel96 <daschechtman@gmail.com>
2021-04-03 02:44:10 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_proj_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
